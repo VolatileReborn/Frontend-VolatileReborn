539bc90c43958f857b1493f808a81b2c1c7f1e98
chenzihong-gavin <191250014@smail.nju.edu.cn>
2022-10-01 09:27:56 +0800

    feat: add tables

--- src/views/status/Status.vue
+++ src/views/status/Status.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        test
-    </div>
+    <el-col :span="12">
+      <h5 style="text-align: center">状态信息</h5>
+      <el-menu
+        default-active="1">
+        <el-menu-item index="1">
+          <span>发包方</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <span>众包工人</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <span>设备</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
 </template>
-
-<script>
-export default {
-    name: "AllStatus",
-
-}
-</script>
+  
+  <script>
+    export default {
+      name:"AllStatus",
+      methods: {
+      }
+    }
+  </script>
